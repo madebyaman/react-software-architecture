1e11713aea0a91e3e4f868f5fa0aadbe273f8111
Aman Thakur <builtbyaman@gmail.com>
2022-02-17 23:28:47 +0530

    when to code split

---
+++ README.md
@@ -0,0 +1,4 @@
+# When to use code splitting?
+
+- Split the code for pages, generally, viewed by users together.
+- Things users won't see immediately.
--- src/pages/About.js
+++ src/pages/About.js
@@ -4,14 +4,19 @@ const Two = lazy(() => import('./Two'));
 const Three = lazy(() => import('./Three'));
 
 const About = () => {
+  const [showComponent, setShowComponent] = React.useState(false);
+
   return (
     <>
       <h1>About</h1>
-      <Suspense fallback={<p>Loading...</p>}>
-        <One />
-        <Two />
-        <Three />
-      </Suspense>
+      <button onClick={() => setShowComponent(true)}>Show components</button>
+      {showComponent && (
+        <Suspense fallback={<p>Loading...</p>}>
+          <One />
+          <Two />
+          <Three />
+        </Suspense>
+      )}
     </>
   );
 };
